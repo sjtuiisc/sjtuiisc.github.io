7b2786e70125518c6506a9a31aa741b2904c8897
sjtuiisc <sjtuiisc@gmail.com>
2025-06-06 05:55:33 +0000

    Deploying to gh-pages from @ sjtuiisc/sjtuiisc.github.io@a6e0ebb6c370969e81f12574ef34c8f78fda433b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,7 +1,7 @@
 
 const currentUrl = window.location.href;
-const siteUrl = "http://sjtuiisc.github.io"; 
-let updatedUrl = currentUrl.replace("http://sjtuiisc.github.io", "");
+const siteUrl = "https://sjtuiisc.github.io"; 
+let updatedUrl = currentUrl.replace("https://sjtuiisc.github.io", "");
 if (currentUrl.length == updatedUrl.length && currentUrl.startsWith("http://127.0.0.1")) {
   const otherSiteUrl = siteUrl.replace("localhost", "127.0.0.1");
   updatedUrl = currentUrl.replace(otherSiteUrl + "", "");
